ff61eb5a5288e63463649864785e2f621506fc23
Md-Siam07 <48762084+Md-Siam07@users.noreply.github.com>
2021-03-13 03:32:34 +0600

    parser added to detect the pressed websites
    parser works using pattern matching algo (KMP algorithm)

--- Updates/main.cpp
+++ Updates/main.cpp
@@ -44,6 +44,133 @@ void authentication();
 char recipients_mail[80];
 int mailMyLog(char *);
 
+string convertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
+vector<int> myPatternMatcher(const char* X, const char* Y, int m, int n)
+{
+    vector<int> ret;
+    // base case 1: `Y` is NULL or empty
+    if (*Y == '\0' || n == 0) {
+        printf("The pattern occurs with shift 0");
+    }
+
+    // base case 2: `X` is NULL, or X's length is less than that of Y's
+    if (*X == '\0' || n > m) {
+        //printf("Pattern not found");
+    }
+
+    // `next[i]` stores the index of the next best partial match
+    int next[n + 1];
+
+    for (int i = 0; i < n + 1; i++) {
+        next[i] = 0;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int j = next[i + 1];
+
+        while (j > 0 && Y[j] != Y[i]) {
+            j = next[j];
+        }
+
+        if (j > 0 || Y[j] == Y[i]) {
+            next[i + 1] = j + 1;
+        }
+    }
+
+    for (int i = 0, j = 0; i < m; i++)
+    {
+        if (*(X + i) == *(Y + j))
+        {
+            if (++j == n) {
+                printf("The pattern occurs with shift %d\n", i - j + 1);
+                ret.push_back(i-j+1);
+            }
+        }
+        else if (j > 0)
+        {
+            j = next[j];
+            i--;    // since `i` will be incremented in the next iteration
+        }
+    }
+    return ret;
+}
+
+void parse()
+{
+    FILE *fp = fopen("top_level_domain.txt","r");
+    char domain[100][100];
+    char web[100][100];
+    int domainN=0;
+    while(fgets(domain[domainN],100,fp)!=0)
+    {
+
+        int len = strlen(domain[domainN]);
+        domain[domainN][len-1]='\0';
+        //cout<< domain[i];
+        domainN++;
+    }
+    fclose(fp);
+
+    FILE *log = fopen("log.txt","r");
+    FILE *wFile = fopen("browsed_websites.txt","a");
+    char buffer[5120];
+
+    while(fgets(buffer,5120,log)!=0)
+    {
+        int bufN = strlen(buffer);
+        cout<< buffer<<endl;
+        for(int i=0; i<domainN;i++)
+        {
+            vector<int> matched = myPatternMatcher(buffer,domain[i],strlen(buffer),strlen(domain[i]));
+            if(matched.size())
+            {
+                for(int it=0; it<matched.size(); it++)
+                {
+                    int index = matched[it];
+                    string dom = convertToString(domain[i], strlen(domain[i]));
+                    string parsedWebsite = dom;
+                    parsedWebsite += "\n";
+                    for(int j = index-1; j>=0; j--)
+                    {
+                        if(j>=2 && buffer[j]== 'w' && buffer[j-1] == 'w' && buffer[j-2] == 'w')
+                        {
+                            parsedWebsite = "www"+parsedWebsite;
+                            break;
+                        }
+
+                        if(buffer[j]== ' ' || buffer[j]== ']' || buffer[j]=='\'' || buffer[j] == '\"' || buffer[j]== '}')
+                        {
+                            break;
+                        }
+
+                        parsedWebsite = buffer[j]+ parsedWebsite;
+                    }
+                    cout<< parsedWebsite;
+                    char toWrite[100];
+                    for(int ii =0;ii< parsedWebsite.size();ii++)
+                    {
+                        toWrite[ii]=parsedWebsite[ii];
+                        cout<< toWrite[ii];
+                    }
+                    toWrite[parsedWebsite.size()]='\0';
+                    fputs(toWrite,wFile);
+                }
+            }
+        }
+    }
+    fclose(log);
+    fclose(wFile);
+}
 
 bool authentication_ret(char takenName[], char takenPass[])
 {
@@ -192,6 +319,7 @@ void viewBrowsedWebsites()
     initwindow(screenWidth,screenHeight,"View website");
     setcolor(WHITE);
     char s[]= "siam";
+    parse();
     FILE *fp = fopen("browsed_websites.txt","r");
     char web[100][100];
     int i=0;
